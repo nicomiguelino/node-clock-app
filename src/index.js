adcb8ee1d144acb05df29105483dd10d9d16c7f7
nicomiguelino <nicomiguelino2014@gmail.com>
2023-09-11 20:22:25 -0700

    fix: Make use of an `index.html` template.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import _ from 'lodash';
 
-function component() {
+function h3({ text }) {
   const element = document.createElement('h3');
-  element.innerHTML = 'Weeeeee!';
+  element.innerHTML = text;
+
   return element;
 }
 
-document.body.appendChild(component());
+function p({ text }) {
+  const element = document.createElement('p');
+  element.innerHTML = text;
+
+  return element;
+}
+
+const container = document.createElement('div');
+const header = h3({ text: 'Hello, Galaxy' });
+
+container.appendChild(header);
+
+[
+  'Kiss me, under the bearded barley',
+  'Nightly, beside the green, green grass',
+  'Swing, swing, swing the spinning step',
+  'You wear those shoes and I will wear that dress',
+  'Oh, kiss me beneath the milky twilight',
+  'Lead me out on the moonlit floor',
+  'Lift your open hand',
+  'Strike up the band and make the fireflies dance',
+  'Silver moon\'s sparkling',
+  'So kiss me',
+].forEach((text) => {
+  const paragraph = p({ text });
+  container.appendChild(paragraph);
+});
+
+document.body.appendChild(container);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
+const fs = require('fs');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Clock App',
+      template: './src/index.ejs',
+    }),
+    {
+      apply: (compiler) => {
+        compiler.hooks.done.tap('DonePlugin', (stats) => {
+          console.log(__dirname);
+          fs.symlink(
+            `${__dirname}/screenly.yml`,
+            `${__dirname}/dist/screenly.yml`,
+            (err) => {
+              if (err) {
+                console.error(err);
+              }
+
+              console.log('Symlink created!');
+            },
+          );
+        });
+      },
+    },
+  ],
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve('dist'),
+    clean: true,
   },
 };
